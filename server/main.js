15a3dd7fdda59175c6f9b2668bd30b1f95bdabe7
lucas teisseire <teisseire@MacBook-Air-de-lucas.local>
2019-08-12 04:14:13 +0200

    comment server to launch app

--- server/main.js
+++ server/main.js
@@ -4,8 +4,12 @@ import fakeArticles from '../imports/dumb-data/fakeArticles'
 import Articles from '../imports/ui/Articles';
 Meteor.startup(() => {
   console.log(fakeArticles)
+
+  // UNCOMMENT TO INSERT DATA IN DB BEFORE START
+
   // fakeArticles.forEach(article => {
   //   dumbArticles.insert(article)
   // })
+
   console.log(dumbArticles.find().fetch())
 })
